b4922d2fa444f1a02b4dfeac67c95756283ff449
汪自立 <wangzili@wangzilideair.lan>
2016-10-09 20:34:48 +0800

    Initial Commit

---
+++ hello obhective-c/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  hello obhective-c
+//
+//  Created by 汪自立 on 2016/10/9.
+//  Copyright © 2016年 汪自立. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
